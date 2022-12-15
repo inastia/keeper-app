6e6da4197bfceb7e6e2cf807ea70153f6734ead3
nastia <nastia.bigun@gmail.com>
2022-12-15 15:41:16 -0700

    Updates

---
+++ precache-manifest.fa63d612618158be83a7ef2eb4c1384e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83711cf69ba8e7e9e9f9a657839d02ec",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "f058971924f2d78b8eb3",
+    "url": "/keeper-app/static/js/2.0d6bca95.chunk.js"
+  },
+  {
+    "revision": "17cc79313fd50a30ae0b",
+    "url": "/keeper-app/static/js/main.81448f7e.chunk.js"
+  },
+  {
+    "revision": "4091f57d993682202988",
+    "url": "/keeper-app/static/js/runtime-main.3d0ac6f6.js"
+  }
+]);
